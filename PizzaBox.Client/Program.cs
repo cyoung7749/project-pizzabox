efe00eb8f90d27bd49e46aa76403f565d6733eac
chris young <christopher.young@revature.net>
2021-04-14 13:03:05 -0400

    Added Client from lecture example

---
+++ PizzaBox.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System;
+using sc = System.Console; // only do this when you are being clear on the alias 
+
+namespace PizzaBox.Client
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            List<string> storeList = new List<string> { "Store 001", "Store 002" };
+            var stores = new List<Store>{ new Store(), new Store() }; // datatype reference
+            // variables are camelcasing 
+            // using List<string stores instead is choosing an explicit datatype
+            for (var x = 0; x < stores.Count; x += 1)
+            {
+                // sc.WriteLine(x + " " + stores[x]);  //this is the old way
+                Console.WriteLine($"{x} - {stores[x]}");
+            }
+            System.Console.WriteLine("make a selection: ");
+            string input = sc.ReadLine();
+            int entry = int.Parse(input);
+            sc.WriteLine(stores[entry]);
+        }
+    }
+}
